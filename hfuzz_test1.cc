f29cb41fbe7de0bfc4cbecf0e87200d160057efa
andypower2016 <73726447+andypower2016@users.noreply.github.com>
2022-10-30 11:50:49 +0800

    Create hfuzz_test1.cc

---
+++ hfuzz_test1.cc
@@ -0,0 +1,66 @@
+// C 
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+// C++
+#include <fstream>
+#include <chrono>
+#include <ctime> 
+#include <sstream>
+#include <iomanip>
+
+std::ofstream outfile;
+
+
+void func(const uint8_t *Data, size_t Size) {
+  struct data{
+    int x;
+  };
+  outfile << Size << std::endl;
+  if(Size > 0)
+  {
+    outfile << "Data:" << std::endl;
+    for (int i = 0 ; i < Size ; ++i)
+    {
+      char buffer[256];
+      sprintf(buffer, "0x%2x\n", Data[i]);
+      outfile << buffer;
+
+      //struct data *ptr_data = NULL;
+      //printf("ptr_data->x=%d\n", ptr_data->x);
+    }
+  }
+}
+
+void CreateCurLogFile()
+{
+  auto t = std::time(nullptr);
+  auto tm = *std::localtime(&t);
+  std::ostringstream oss;
+  oss << std::put_time(&tm, "%d-%m-%Y_%H-%M-%S");
+  auto str = oss.str();
+
+  char filename[256];
+  sprintf(filename, "./myout/fout_%d_%s", getpid(), str.c_str());
+  outfile.open(filename, std::ofstream::out);
+}
+
+void CloseCurLogFile()
+{
+  outfile.close();
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+
+  CreateCurLogFile();
+  func(Data, Size);
+  CloseCurLogFile();
+  return 0;
+}
+/*
+int main()
+{
+	printf("main\n");
+	return 0;
+}*/
